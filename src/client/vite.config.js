b284d5b777430954f80f38859c0bd0205c46f521
Shachar <shachar64@gmail.com>
2022-12-03 19:25:03 +0200

    Proxy config.

--- src/client/vite.config.js
+++ src/client/vite.config.js
@@ -3,8 +3,24 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false
+
+      },
+      "/auth": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false
+
+      }
+    }
+  },
   plugins: [vue()],
   build: {
-    outDir: '../src/dist'
+    outDir: './dist'
   }
 })
